255e77a7e2847043efcd11ddb1413f0d1af231b0
David BARAN <david.baran@everial.com>
2019-09-19 17:56:31 +0200

    Fixes docker port mapping

--- start_in_docker.sh
+++ start_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --name node -p 8000:3000 -v "$PWD":/usr/src/app -w /usr/src/app node:current-alpine npm run start
+docker run -it --rm --name node -p 8000:8000 -v "$PWD":/usr/src/app -w /usr/src/app node:current-alpine npm run start
